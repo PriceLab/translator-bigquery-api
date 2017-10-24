1a8dac8add4b59a2726de8af901de4ea41770baa
Debian <admin@ip-10-1-2-21.ec2.internal>
2017-10-24 22:47:40 +0000

    Mostly working

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 
 
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    #flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
--- docker/API/start_service.sh
+++ docker/API/start_service.sh
@@ -2,9 +2,9 @@
 
 IMAGE=pricelab/translator-bigquery-api
 APP_DIR=/home/admin/translator-bigquery-api/app
-CRED_DIR=/home/admin/bq_cred
+CRED_DIR=/home/admin/cred
 
-docker run -d --name translator-bigquery-app -p 80:80 \
+docker run -d --name translator-bigquery-app -p 80:80 -p 443:443 \
         -v $APP_DIR:/app \
         -v $CRED_DIR:/cred \
         $IMAGE
